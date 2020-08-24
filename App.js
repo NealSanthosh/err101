917ee2fd6afb53c45bd4989d699e9521141d9ad0
SansNeal <62467577+SansNeal@users.noreply.github.com>
2020-08-24 16:43:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <View>
+        <WelcomeScreen/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
